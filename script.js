5dc224f760b2757b72e9588d7eacab697427e8ed
Pasha-007 <m.muntahaa@gmail.com>
2022-12-03 14:51:05 +0600

    Commit to update landing page

---
+++ script.js
@@ -0,0 +1,9 @@
+let hamburger = document.getElementById('hamburger')
+menu = document.getElementById('menu')
+header = document.getElementById('header')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active')
+    menu.classList.toggle('active')
+})
+
